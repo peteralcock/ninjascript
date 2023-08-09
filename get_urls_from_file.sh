9514a0c8b499182c18a68c715ec74a25f2f70d82
Peter Alcock <peteralcock@users.noreply.github.com>
2023-08-09 04:59:02 -0400

    Update get_urls_from_file.sh

--- get_urls_from_file.sh
+++ get_urls_from_file.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 TARGET_FILE=$1
-grep -Eoi '<a [^>]+>' $TARGET_FILE | grep -Eo 'href="[^\"]+”' |grep -Eo '(http|https)://[^”]+'
+grep -Eoi '<a [^>]+>' $TARGET_FILE | grep -Eo 'href="[^\"]+”' | grep -Eo '(http|https)://[^”]+'
